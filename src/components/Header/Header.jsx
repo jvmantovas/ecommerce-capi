f532c5d84e33ad86621a34fb2740426164fff784
jvmantovas <vmsjoao@gmail.com>
2022-12-12 10:55:05 -0300

    basic router settings

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Button from "../Button/Button";
 import { LowerHeader, MainHeader, UpperHeader } from "./styles";
 
@@ -9,6 +10,14 @@ function Header() {
       <MainHeader>
         <Button />
         <img src="../../../public/capi-logo.svg" alt="" />
+        <div>
+          <Link to="/">
+            <img src="../../../public/acc.svg" alt="" />
+          </Link>
+          <Link to="/">
+            <img src="../../../public/cart.svg" alt="" />
+          </Link>
+        </div>
       </MainHeader>
       <LowerHeader />
     </header>
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import GlobalStyle from "./styles/globalStyles";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-    <GlobalStyle />
+    <Router>
+      <App />
+      <GlobalStyle />
+    </Router>
   </React.StrictMode>
 );
