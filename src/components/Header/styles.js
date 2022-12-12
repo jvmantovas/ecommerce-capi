a06e04c48e7f385084d15f3c90b41f35754d7841
jvmantovas <vmsjoao@gmail.com>
2022-12-12 11:41:57 -0300

    general header adjustings

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -9,13 +9,32 @@ export const UpperHeader = styled.div`
 export const MainHeader = styled.div`
   height: 72px;
   background-color: #f6d53b;
-  border: 1px solid black;
   display: flex;
   align-items: center;
   justify-content: space-between;
+  z-index: 3;
+  position: relative;
+  padding: 0 2rem;
+  .right-side img {
+    padding: 10px;
+  }
 `;
 
 export const LowerHeader = styled.div`
   height: 46px;
   background-color: #f6d53b;
+  display: flex;
+  justify-content: center;
+  box-shadow: 0px 10px 25px rgba(0, 0, 0, 0.1);
+  z-index: 2;
+  position: relative;
+`;
+
+export const Menu = styled.div`
+  width: 700px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  padding: 10px;
+  font-weight: 500;
 `;
