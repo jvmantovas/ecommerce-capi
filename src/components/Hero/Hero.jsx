84c041688370cafb47fb8333b3e2cdb770c252da
jvmantovas <vmsjoao@gmail.com>
2022-12-12 09:14:12 -0300

    feat(hero) initial main page wireframe

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Main } from "./styles";
+
+function Hero() {
+  return (
+    <main>
+      <Main />
+    </main>
+  );
+}
+
+export default Hero;
---
+++ src/components/Hero/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Main = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #f6d53b;
+  border: 1px solid black;
+`;
