e67a1447696adf4ca48000af05c69fd3ef76f274
jvmantovas <vmsjoao@gmail.com>
2023-01-18 09:44:47 -0300

    feat: login page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { Home } from "./pages/home/home";
+import { Login } from "./pages/login/login";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
