bb3abc2ed63a21228ca02d35e00ecd7465dfb084
João Mantovaneli <109223861+jvmantovas@users.noreply.github.com>
2023-01-19 00:27:40 -0300

    feat: hamburger menu navbar

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,17 +1,21 @@
 import React from "react";
+import { CgMenuLeftAlt } from "react-icons/cg";
 import { FaSearch } from "react-icons/fa";
 import { Form, Input, SearchContainer, Submit } from "./styles";
 
 function Button() {
   return (
-    <SearchContainer>
-      <Form action="">
-        <Input type="text" placeholder="Buscar..." name="search" />
-        <Submit type="submit">
-          <FaSearch />
-        </Submit>
-      </Form>
-    </SearchContainer>
+    <>
+      <CgMenuLeftAlt size={35} display="none" />
+      <SearchContainer>
+        <Form action="">
+          <Input type="text" placeholder="Buscar..." name="search" />
+          <Submit type="submit">
+            <FaSearch />
+          </Submit>
+        </Form>
+      </SearchContainer>
+    </>
   );
 }
 
