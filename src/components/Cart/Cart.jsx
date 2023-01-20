167598e12169fc34b6a4e34e64f4ae81855b719c
jvmantovas <vmsjoao@gmail.com>
2023-01-20 01:08:07 -0300

    feat: cart component layout

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
+import { Main } from "../Hero/styles";
+import {
+  CartWrapper,
+  CartTable,
+  CartButton,
+  CartButtonsWrapper,
+  PaymentButton,
+} from "./styles";
 
 const Cart = () => {
-    const [cartItems, setCartItems] = useState([
-    { id: 1, name: 'Product 1', price: 10, quantity: 2 },
-    { id: 2, name: 'Product 2', price: 15, quantity: 1 },
-    { id: 3, name: 'Product 3', price: 20, quantity: 3 },
+  const [cartItems, setCartItems] = useState([
+    { id: 1, name: "Produto 1", price: 10, quantity: 2 },
+    { id: 2, name: "Produto 2", price: 15, quantity: 1 },
+    { id: 3, name: "Produto 3", price: 20, quantity: 3 },
   ]);
 
   const handleRemoveFromCart = (id) => {
@@ -16,28 +24,50 @@ const Cart = () => {
   };
 
   return (
-    <CartWrapper>
-      <h2>Cart</h2>
-      <CartTable>
-        <thead>
-          <tr>
-            <th>Product</th>
-            <th>Price</th>
-            <th>Quantity</th>
-            <th>Total</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {cartItems.map((item) => (
-            <tr key={item.id}>
-              <td>{item.name}</td>
-              <td>{item.price}</td>
-              <td>{item.quantity}</td>
-              <td>{item.price * item.quantity}</td>
-              <td>
-                <CartButton onClick={() => handleRemoveFromCart(item.id)}>Remove</CartButton>
-              </td>
+    <Main style={{ backgroundColor: "#f6d53b" }}>
+      <CartWrapper>
+        <h2>Carrinho</h2>
+        <CartTable>
+          <thead>
+            <tr>
+              <th>Produto</th>
+              <th>Preço</th>
+              <th>Quantidade</th>
+              <th>Total</th>
+              <th>Remover</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cartItems.map((item) => (
+              <tr key={item.id}>
+                <td>{item.name}</td>
+                <td>R${item.price}</td>
+                <td>{item.quantity}</td>
+                <td>R${item.price * item.quantity}</td>
+                <td className="remove-td">
+                  <CartButton onClick={() => handleRemoveFromCart(item.id)}>
+                    Remover
+                  </CartButton>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </CartTable>
+        <div>
+          <h3>
+            Total: R$
+            {cartItems.reduce(
+              (acc, item) => acc + item.price * item.quantity,
+              0
+            )}
+          </h3>
+          <CartButtonsWrapper>
+            <PaymentButton>Prosseguir para pagamento</PaymentButton>
+            <CartButton onClick={handleEmptyCart}>Esvaziar Carrinho</CartButton>
+          </CartButtonsWrapper>
+        </div>
+      </CartWrapper>
+    </Main>
   );
 };
 
--- src/components/Cart/styles.js
+++ src/components/Cart/styles.js
@@ -1,29 +1,69 @@
 import styled from "styled-components";
 
-const CartWrapper = styled.section`
+export const CartWrapper = styled.section`
   width: 100%;
-  padding: 1rem;
-`;
-
-const CartTable = styled.table`
-  width: 100%;
-  border-collapse: collapse;
-  th,
-  td {
-    border: 1px solid #ddd;
-    padding: 0.5rem;
-    text-align: left;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  padding-top: 3rem;
+  margin-left: 3rem;
+  h2 {
+    font-family: "HarmoniaSansW01-Semibold", sans-serif;
+    font-size: 50px;
+    line-height: 20px;
+    align-self: flex-start;
+    margin-bottom: 3rem;
+  }
+  h3 {
+    padding: 2rem;
   }
 `;
 
-const CartButton = styled.button`
+export const CartTable = styled.table`
+  width: 100%;
+  max-width: 90vw;
+  border-collapse: collapse;
+  th,
+  td {
+    text-align: left;
+    padding: 10px;
+    border-bottom: 1px solid #6b6b6b;
+    font-family: "HarmoniaSansMonoW01-Regular", sans-serif;
+  }
+  tbody {
+    display: table-row-group;
+  }
+  .remove-td {
+    width: 120px;
+  }
+`;
+
+export const CartButtonsWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 90vw;
+`;
+
+export const CartButton = styled.button`
   background-color: #f6d53b;
   color: #000;
   padding: 0.5rem 1rem;
-  border: none;
+  border: 1px solid black;
   cursor: pointer;
   &:hover {
     background-color: #000;
     color: #f6d53b;
+  }
+`;
+
+export const PaymentButton = styled(CartButton)`
+  background-color: #f6d53b;
+  color: #b50016;
+  font-weight: bolder;
+  &:hover {
+    color: #b50016;
   }
 `;
