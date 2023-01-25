0c8a1a9560909315acc4b4eabd04f8c1af0061e1
jvmantovas <vmsjoao@gmail.com>
2023-01-25 10:53:27 -0300

    feat: register form logics

--- src/components/LoginSection/LoginSection.jsx
+++ src/components/LoginSection/LoginSection.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Main } from "../Hero/styles";
 import {
   ActionButton,
@@ -12,27 +12,65 @@ import {
 } from "./styles";
 
 const LoginSection = () => {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    const sendData = {
+      name: data.name,
+      email: data.email,
+      password: data.password,
+    };
+  };
+
   return (
     <Main style={{ backgroundColor: "#f6d53b" }}>
       <MainWrapper>
         <Container>
           <LoginTitle>Login</LoginTitle>
           <FieldName>Email</FieldName>
-          <FieldInput />
+          <FieldInput type="email" name="email" />
           <FieldName>Senha</FieldName>
-          <FieldInput />
+          <FieldInput type="password" name="password" />
           <ActionButton>Entrar</ActionButton>
         </Container>
         <Line />
         <Container>
-          <LoginTitle>Criar conta</LoginTitle>
-          <FieldName>Nome completo</FieldName>
-          <FieldInput />
-          <FieldName>Email</FieldName>
-          <FieldInput />
-          <FieldName>Senha</FieldName>
-          <FieldInput />
-          <ActionButton>Cadastrar</ActionButton>
+          <form onSubmit={submitForm}>
+            <LoginTitle>Criar conta</LoginTitle>
+            <FieldName>Nome completo</FieldName>
+            <FieldInput
+              type="text"
+              name="name"
+              onChange={handleChange}
+              value={data.name}
+            />
+            <FieldName>Email</FieldName>
+            <FieldInput
+              type="email"
+              name="email"
+              onChange={handleChange}
+              value={data.email}
+            />
+            <FieldName>Senha</FieldName>
+            <FieldInput
+              type="password"
+              name="password"
+              onChange={handleChange}
+              value={data.value}
+            />
+            <ActionButton type="submit" name="submit" value="Register">
+              Cadastrar
+            </ActionButton>
+          </form>
         </Container>
       </MainWrapper>
     </Main>
