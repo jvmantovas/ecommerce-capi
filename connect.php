9b9157825bfb657ee71c2174bac9f92e470450f1
jvmantovas <vmsjoao@gmail.com>
2023-01-26 15:38:03 -0300

    php connection to mysql

--- connect.php
+++ connect.php
@@ -5,7 +5,7 @@ $password = "";
 $database= "capi";
  
 $db = mysqli_connect($servername, $username, $password, $database);
- 
+
 if ($db->connect_error) {
   die("Connection failed: " . $db->connect_error);
 }
