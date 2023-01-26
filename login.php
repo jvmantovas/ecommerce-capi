576d79eea1e22c56dae841c284ef2955e304ba83
jvmantovas <vmsjoao@gmail.com>
2023-01-26 15:37:49 -0300

    feat: login backend

---
+++ login.php
@@ -0,0 +1,31 @@
+<?php
+require "connect.php";
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+$postdata = file_get_contents("php://input");
+if(isset($postdata) && !empty($postdata)) {
+    $request = json_decode($postdata);
+
+    $email = $request->email;
+    $password = $request->password;
+    
+
+    $sql = ("SELECT * from users where email = '$email' and password = '$password'");
+    $result = mysqli_query($db, $sql);
+
+    $nums = mysqli_num_rows($result);
+    $rs = mysqli_fetch_array($result);
+
+    if($nums != 0) {
+        http_response_code(200);
+
+    } else {
+        http_response_code(202);
+    }
+}
+?>
