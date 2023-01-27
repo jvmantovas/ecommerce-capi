4b2299a032f43f2dac22f1b229ef1ef88265bef0
jvmantovas <vmsjoao@gmail.com>
2023-01-27 09:57:04 -0300

    feat: featch products and json object from db

---
+++ products.php
@@ -0,0 +1,20 @@
+<?php
+require "connect.php";
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+$query = $db->query("SELECT * FROM products");
+
+$products = [];
+
+while($row = $query->fetch_assoc()) {
+    $products[] = $row;
+};
+
+echo json_encode($products);
+
+?>
