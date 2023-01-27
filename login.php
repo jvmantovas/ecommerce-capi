d6240698f74643dd6084abd532c286985199a8f6
jvmantovas <vmsjoao@gmail.com>
2023-01-27 19:43:29 -0300

    fix: duplicate header origin

--- login.php
+++ login.php
@@ -1,6 +1,5 @@
 <?php
 require "connect.php";
-header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Headers: access");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
