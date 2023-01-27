9bce384fa824d15d79b8e367ee723f2b60519372
jvmantovas <vmsjoao@gmail.com>
2023-01-27 19:43:58 -0300

    update: improved error handling

--- products.php
+++ products.php
@@ -9,12 +9,22 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 
 $query = $db->query("SELECT * FROM product");
 
+if (!$query) {
+    http_response_code(500);
+    die(json_encode(array("message" => "Failed to retrieve products")));
+}
+
 $products = [];
 
 while($row = $query->fetch_assoc()) {
     $products[] = $row;
 };
 
-echo json_encode($products);
+if (!empty($products)) {
+    echo json_encode($products);
+} else {
+    http_response_code(404);
+    die(json_encode(array("message" => "No products found")));
+}
 
 ?>
