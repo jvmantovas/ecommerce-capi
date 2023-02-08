f592f90604a99a644079a61079d85ad6070401ca
jvmantovas <vmsjoao@gmail.com>
2023-02-08 11:25:54 -0300

    feat: display products with fixed filters

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,42 +1,34 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Product } from "../Product/Product";
 import { useProductData } from "../../hooks/useProductData";
-import { Filters } from "../Filters/Filters";
 
-const ProductList = () => {
+const ProductList = ({ filterType, genre }) => {
   const products = useProductData();
   const [filteredProducts, setFilteredProducts] = useState([]);
-  const [filter, setFilter] = useState({ genre: "", promoChecked: true });
 
   useEffect(() => {
     let filteredProducts = products;
 
-    if (filter.genre) {
+    if (filterType === "promo") {
       filteredProducts = products.filter(
-        (product) =>
-          product.first_genre === filter.genre ||
-          product.second_genre === filter.genre ||
-          product.first_subgenre === filter.genre ||
-          product.second_subgenre === filter.genre
-      );
-    }
-
-    if (filter.promoChecked) {
-      filteredProducts = filteredProducts.filter(
         (product) => product.old_price !== "0.00"
       );
+    } else if (filterType === "genre") {
+      filteredProducts = products.filter(
+        (product) =>
+          product.first_genre === genre ||
+          product.second_genre === genre ||
+          product.first_subgenre === genre ||
+          product.second_subgenre === genre
+      );
     }
 
-    setFilteredProducts(filteredProducts);
-  }, [filter, products]);
+    filteredProducts = filteredProducts.slice(0, 10);
 
-  return (
-    <>
-      {filteredProducts.slice(0, 10).map((product) => (
-        <Product products={filteredProducts} />
-      ))}
-    </>
-  );
+    setFilteredProducts(filteredProducts);
+  }, [filterType, genre, products]);
+
+  return <Product products={filteredProducts} />;
 };
 
 export { ProductList };
