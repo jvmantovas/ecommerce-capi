f4681041be9f13205e7164db904d6b52ecef04cc
jvmantovas <vmsjoao@gmail.com>
2023-02-08 11:25:11 -0300

    refactor: cleaning and ProductList usage

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,24 +1,15 @@
 import React, { useState } from "react";
 import {
-  AlbumTitle,
-  Card,
   CardsWrapper,
-  Cover,
   LeftArrowDiv,
   Main,
-  MainGenre,
-  OldPrice,
-  Price,
-  PriceWrapper,
-  RecordType,
   RightArrowDiv,
   SeeMore,
-  SubGenre,
   Title,
 } from "./styles";
-import { Product } from "../Product/Product";
 import { SlArrowLeft, SlArrowRight } from "react-icons/sl";
 import { useRef } from "react";
+import { ProductList } from "../ProductList/ProductList";
 
 const Hero = () => {
   const [scrollLeft, setScrollLeft] = useState(0);
@@ -32,16 +23,18 @@ const Hero = () => {
     setShowRightArrow(scrollWidth > clientWidth + scrollLeft);
   };
   const [scrollPosition, setScrollPosition] = useState(0);
-  const cardsWrapperRef = useRef(null);
+  const cardsWrapperRef1 = useRef(null);
+  const cardsWrapperRef2 = useRef(null);
+  const cardsWrapperRef3 = useRef(null);
 
-  const handleLeftArrowClick = () => {
+  const handleLeftArrowClick = (ref) => {
     setScrollPosition(scrollPosition - 100);
-    cardsWrapperRef.current.scrollLeft -= 200;
+    ref.current.scrollLeft -= 200;
   };
 
-  const handleRightArrowClick = () => {
+  const handleRightArrowClick = (ref) => {
     setScrollPosition(scrollPosition + 100);
-    cardsWrapperRef.current.scrollLeft += 200;
+    ref.current.scrollLeft += 200;
   };
 
   return (
@@ -53,20 +46,18 @@ const Hero = () => {
           <CardsWrapper
             onScroll={handleScroll}
             scrollLeft={scrollLeft}
-            ref={cardsWrapperRef}
+            ref={cardsWrapperRef1}
           >
             <LeftArrowDiv
-              onClick={handleLeftArrowClick}
-              // onClick={() => setScrollLeft(scrollLeft - 300)}
+              onClick={() => handleLeftArrowClick(cardsWrapperRef1)}
               show={showLeftArrow}
             >
               <SlArrowLeft size={45} style={{ color: "#444444" }} />
             </LeftArrowDiv>
-            <Product />
+            <ProductList filterType="promo" />
             <RightArrowDiv
-              // onClick={() => setScrollLeft(scrollLeft + 300)}
               show={showRightArrow}
-              onClick={handleRightArrowClick}
+              onClick={() => handleRightArrowClick(cardsWrapperRef1)}
               style={{ color: "#444444" }}
             >
               <SlArrowRight size={45} />
@@ -76,181 +67,49 @@ const Hero = () => {
         <Main style={{ backgroundColor: "#CE2237" }}>
           <Title style={{ color: "#f6d53b" }}>NOVIDADES</Title>
           <SeeMore>Ver todos</SeeMore>
-          <CardsWrapper>
-            <Card>
-              <Cover
-                src="https://i.discogs.com/e8XliqJxlmtY2ES7-X9l9hihTTU9RQzL8Ecu6eWsQuA/rs:fit/g:sm/q:90/h:597/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTc1MzM1/NC0xNjI0OTA4NTI3/LTM1NTcuanBlZw.jpeg"
-                alt=""
-              />
-              <AlbumTitle>Jorge Ben - Jorge Ben</AlbumTitle>
-              <MainGenre>Jazz, Brasil</MainGenre>
-              <SubGenre>Bossanova, MPB</SubGenre>
-              <OldPrice>R$159,00</OldPrice>
-              <PriceWrapper>
-                <RecordType>VINIL - LP</RecordType>
-                <Price>R$130,00</Price>
-              </PriceWrapper>
-            </Card>
-            <Card>
-              <Cover
-                src="https://i.discogs.com/e8XliqJxlmtY2ES7-X9l9hihTTU9RQzL8Ecu6eWsQuA/rs:fit/g:sm/q:90/h:597/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTc1MzM1/NC0xNjI0OTA4NTI3/LTM1NTcuanBlZw.jpeg"
-                alt=""
-              />
-              <AlbumTitle>Jorge Ben - Jorge Ben</AlbumTitle>
-              <MainGenre>Jazz, Brasil</MainGenre>
-              <SubGenre>Bossanova, MPB</SubGenre>
-              <OldPrice>R$159,00</OldPrice>
-              <PriceWrapper>
-                <RecordType>VINIL - LP</RecordType>
-                <Price>R$130,00</Price>
-              </PriceWrapper>
-            </Card>
-            <Card>
-              <Cover
-                src="https://i.discogs.com/e8XliqJxlmtY2ES7-X9l9hihTTU9RQzL8Ecu6eWsQuA/rs:fit/g:sm/q:90/h:597/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTc1MzM1/NC0xNjI0OTA4NTI3/LTM1NTcuanBlZw.jpeg"
-                alt=""
-              />
-              <AlbumTitle>Jorge Ben - Jorge Ben</AlbumTitle>
-              <MainGenre>Jazz, Brasil</MainGenre>
-              <SubGenre>Bossanova, MPB</SubGenre>
-              <OldPrice>R$159,00</OldPrice>
-              <PriceWrapper>
-                <RecordType>VINIL - LP</RecordType>
-                <Price>R$130,00</Price>
-              </PriceWrapper>
-            </Card>
-            <Card>
-              <Cover
-                src="https://i.discogs.com/e8XliqJxlmtY2ES7-X9l9hihTTU9RQzL8Ecu6eWsQuA/rs:fit/g:sm/q:90/h:597/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTc1MzM1/NC0xNjI0OTA4NTI3/LTM1NTcuanBlZw.jpeg"
-                alt=""
-              />
-              <AlbumTitle>Jorge Ben - Jorge Ben</AlbumTitle>
-              <MainGenre>Jazz, Brasil</MainGenre>
-              <SubGenre>Bossanova, MPB</SubGenre>
-              <OldPrice>R$159,00</OldPrice>
-              <PriceWrapper>
-                <RecordType>VINIL - LP</RecordType>
-                <Price>R$130,00</Price>
-              </PriceWrapper>
-            </Card>
-            <Card>
-              <Cover
-                src="https://i.discogs.com/e8XliqJxlmtY2ES7-X9l9hihTTU9RQzL8Ecu6eWsQuA/rs:fit/g:sm/q:90/h:597/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTc1MzM1/NC0xNjI0OTA4NTI3/LTM1NTcuanBlZw.jpeg"
-                alt=""
-              />
-              <AlbumTitle>Jorge Ben - Jorge Ben</AlbumTitle>
-              <MainGenre>Jazz, Brasil</MainGenre>
-              <SubGenre>Bossanova, MPB</SubGenre>
-              <OldPrice>R$159,00</OldPrice>
-              <PriceWrapper>
-                <RecordType>VINIL - LP</RecordType>
-                <Price>R$130,00</Price>
-              </PriceWrapper>
-            </Card>
-            <Card>
-              <Cover
-                src="https://i.discogs.com/e8XliqJxlmtY2ES7-X9l9hihTTU9RQzL8Ecu6eWsQuA/rs:fit/g:sm/q:90/h:597/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTc1MzM1/NC0xNjI0OTA4NTI3/LTM1NTcuanBlZw.jpeg"
-                alt=""
-              />
-              <AlbumTitle>Jorge Ben - Jorge Ben</AlbumTitle>
-              <MainGenre>Jazz, Brasil</MainGenre>
-              <SubGenre>Bossanova, MPB</SubGenre>
-              <OldPrice>R$159,00</OldPrice>
-              <PriceWrapper>
-                <RecordType>VINIL - LP</RecordType>
-                <Price>R$130,00</Price>
-              </PriceWrapper>
-            </Card>
+          <CardsWrapper
+            onScroll={handleScroll}
+            scrollLeft={scrollLeft}
+            ref={cardsWrapperRef2}
+          >
+            <LeftArrowDiv
+              onClick={() => handleLeftArrowClick(cardsWrapperRef2)}
+              show={showLeftArrow}
+            >
+              <SlArrowLeft size={45} style={{ color: "#444444" }} />
+            </LeftArrowDiv>
+            <ProductList filterType="promo" />
+            <RightArrowDiv
+              show={showRightArrow}
+              onClick={() => handleRightArrowClick(cardsWrapperRef2)}
+              style={{ color: "#444444" }}
+            >
+              <SlArrowRight size={45} />
+            </RightArrowDiv>
           </CardsWrapper>
         </Main>
         <Main style={{ backgroundColor: "#f6d53b" }}>
           <Title style={{ color: "#CE2237" }}>DESTAQUES</Title>
           <SeeMore>Ver todos</SeeMore>
-          <CardsWrapper>
-            <Card>
-              <Cover
-                src="https://i.discogs.com/e8XliqJxlmtY2ES7-X9l9hihTTU9RQzL8Ecu6eWsQuA/rs:fit/g:sm/q:90/h:597/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTc1MzM1/NC0xNjI0OTA4NTI3/LTM1NTcuanBlZw.jpeg"
-                alt=""
-              />
-              <AlbumTitle>Jorge Ben - Jorge Ben</AlbumTitle>
-              <MainGenre>Jazz, Brasil</MainGenre>
-              <SubGenre>Bossanova, MPB</SubGenre>
-              <OldPrice>R$159,00</OldPrice>
-              <PriceWrapper>
-                <RecordType>VINIL - LP</RecordType>
-                <Price>R$130,00</Price>
-              </PriceWrapper>
-            </Card>
-            <Card>
-              <Cover
-                src="https://i.discogs.com/e8XliqJxlmtY2ES7-X9l9hihTTU9RQzL8Ecu6eWsQuA/rs:fit/g:sm/q:90/h:597/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTc1MzM1/NC0xNjI0OTA4NTI3/LTM1NTcuanBlZw.jpeg"
-                alt=""
-              />
-              <AlbumTitle>Jorge Ben - Jorge Ben</AlbumTitle>
-              <MainGenre>Jazz, Brasil</MainGenre>
-              <SubGenre>Bossanova, MPB</SubGenre>
-              <OldPrice>R$159,00</OldPrice>
-              <PriceWrapper>
-                <RecordType>VINIL - LP</RecordType>
-                <Price>R$130,00</Price>
-              </PriceWrapper>
-            </Card>
-            <Card>
-              <Cover
-                src="https://i.discogs.com/e8XliqJxlmtY2ES7-X9l9hihTTU9RQzL8Ecu6eWsQuA/rs:fit/g:sm/q:90/h:597/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTc1MzM1/NC0xNjI0OTA4NTI3/LTM1NTcuanBlZw.jpeg"
-                alt=""
-              />
-              <AlbumTitle>Jorge Ben - Jorge Ben</AlbumTitle>
-              <MainGenre>Jazz, Brasil</MainGenre>
-              <SubGenre>Bossanova, MPB</SubGenre>
-              <OldPrice>R$159,00</OldPrice>
-              <PriceWrapper>
-                <RecordType>VINIL - LP</RecordType>
-                <Price>R$130,00</Price>
-              </PriceWrapper>
-            </Card>
-            <Card>
-              <Cover
-                src="https://i.discogs.com/e8XliqJxlmtY2ES7-X9l9hihTTU9RQzL8Ecu6eWsQuA/rs:fit/g:sm/q:90/h:597/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTc1MzM1/NC0xNjI0OTA4NTI3/LTM1NTcuanBlZw.jpeg"
-                alt=""
-              />
-              <AlbumTitle>Jorge Ben - Jorge Ben</AlbumTitle>
-              <MainGenre>Jazz, Brasil</MainGenre>
-              <SubGenre>Bossanova, MPB</SubGenre>
-              <OldPrice>R$159,00</OldPrice>
-              <PriceWrapper>
-                <RecordType>VINIL - LP</RecordType>
-                <Price>R$130,00</Price>
-              </PriceWrapper>
-            </Card>
-            <Card>
-              <Cover
-                src="https://i.discogs.com/e8XliqJxlmtY2ES7-X9l9hihTTU9RQzL8Ecu6eWsQuA/rs:fit/g:sm/q:90/h:597/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTc1MzM1/NC0xNjI0OTA4NTI3/LTM1NTcuanBlZw.jpeg"
-                alt=""
-              />
-              <AlbumTitle>Jorge Ben - Jorge Ben</AlbumTitle>
-              <MainGenre>Jazz, Brasil</MainGenre>
-              <SubGenre>Bossanova, MPB</SubGenre>
-              <OldPrice>R$159,00</OldPrice>
-              <PriceWrapper>
-                <RecordType>VINIL - LP</RecordType>
-                <Price>R$130,00</Price>
-              </PriceWrapper>
-            </Card>
-            <Card>
-              <Cover
-                src="https://i.discogs.com/e8XliqJxlmtY2ES7-X9l9hihTTU9RQzL8Ecu6eWsQuA/rs:fit/g:sm/q:90/h:597/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTc1MzM1/NC0xNjI0OTA4NTI3/LTM1NTcuanBlZw.jpeg"
-                alt=""
-              />
-              <AlbumTitle>Jorge Ben - Jorge Ben</AlbumTitle>
-              <MainGenre>Jazz, Brasil</MainGenre>
-              <SubGenre>Bossanova, MPB</SubGenre>
-              <OldPrice>R$159,00</OldPrice>
-              <PriceWrapper>
-                <RecordType>VINIL - LP</RecordType>
-                <Price>R$130,00</Price>
-              </PriceWrapper>
-            </Card>
+          <CardsWrapper
+            onScroll={handleScroll}
+            scrollLeft={scrollLeft}
+            ref={cardsWrapperRef3}
+          >
+            <LeftArrowDiv
+              onClick={() => handleLeftArrowClick(cardsWrapperRef3)}
+              show={showLeftArrow}
+            >
+              <SlArrowLeft size={45} style={{ color: "#444444" }} />
+            </LeftArrowDiv>
+            <ProductList filterType="promo" />
+            <RightArrowDiv
+              show={showRightArrow}
+              onClick={() => handleRightArrowClick(cardsWrapperRef3)}
+              style={{ color: "#444444" }}
+            >
+              <SlArrowRight size={45} />
+            </RightArrowDiv>
           </CardsWrapper>
         </Main>
       </main>
