affe79063e28d6b19bdaa5d5c45762d8079c1d10
jvmantovas <vmsjoao@gmail.com>
2023-03-05 00:11:23 -0300

    feat: token localstorage auth logics

--- login.php
+++ login.php
@@ -6,6 +6,8 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
+session_start();
+
 $postdata = file_get_contents("php://input");
 if(isset($postdata) && !empty($postdata)) {
     $request = json_decode($postdata);
@@ -22,22 +24,44 @@ if(isset($postdata) && !empty($postdata)) {
     $rs = mysqli_fetch_array($result);
 
     if($nums != 0) {
-        http_response_code(200);
-         $data = [
-            "status" => "success",
-            "user" => [
-                "name" => $rs['name'],
-                "email" => $rs['email']
-            ]
-        ];
-        echo json_encode($data);
+         // Generate random token for user
+        $token = bin2hex(random_bytes(32));
+        $expires_at = date('Y-m-d H:i:s', strtotime('+1 week'));
+        $user_id = $rs['id'];
+        
+        // Insert token into user_tokens table
+        $stmt = mysqli_prepare($db, "INSERT INTO user_tokens (user_id, token, expires_at) VALUES (?, ?, ?)");
+        mysqli_stmt_bind_param($stmt, 'iss', $user_id, $token, $expires_at);
+        if (mysqli_stmt_execute($stmt)) {
 
-    } else {
+            http_response_code(200);
+            $data = [
+                "status" => "success",
+                "user" => [
+                    "id" => $rs['id'],
+                    "name" => $rs['name'],
+                    "email" => $rs['email']
+                ],
+                "token" => $token // add token to response
+            ];
+
+            echo json_encode($data);
+
+        } else {
+            http_response_code(401);
+            $data = [
+                "status" => "failure",
+                "message" => "Invalid email or password"
+            ];
+            echo json_encode($data);
+        }
+    }   else {
         http_response_code(401);
         $data = [
             "status" => "failure",
             "message" => "Invalid email or password"
         ];
+        echo json_encode($data);
     }
 }
 ?>
