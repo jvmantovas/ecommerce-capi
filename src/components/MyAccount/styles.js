55f9794d0ade94a200fb3c64fd9ab403d8808c6c
jvmantovas <vmsjoao@gmail.com>
2023-03-06 00:15:22 -0300

    styles: responsivity and logout button improvs

--- src/components/MyAccount/styles.js
+++ src/components/MyAccount/styles.js
@@ -3,10 +3,9 @@ import styled from "styled-components";
 export const AccountWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
-  justify-content: flex-start;
-  padding-top: 3rem;
-  margin-left: 3rem;
+  align-items: center;
+  justify-content: space-between;
+  padding: 3rem;
   h2 {
     font-family: "HarmoniaSansW01-Semibold", sans-serif;
     font-size: 50px;
@@ -17,34 +16,90 @@ export const AccountWrapper = styled.div`
   h3 {
     padding: 2rem;
   }
+  @media screen and (max-width: 768px) {
+    padding: 2rem;
+  }
+`;
+
+export const InfoWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  min-width: 100%;
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+export const CartInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  h2 {
+    font-family: "HarmoniaSansW01-Semibold", sans-serif;
+    font-size: 50px;
+    line-height: 20px;
+    align-self: flex-start;
+    margin-bottom: 3rem;
+  }
+  h3 {
+    padding: 2rem;
+  }
+  @media screen and (max-width: 768px) {
+    margin-bottom: 2rem;
+  }
 `;
 
 export const UserInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   p {
     font-size: 20px;
     margin: 0.5rem 0;
+  }
+
+  @media screen and (max-width: 768px) {
+    margin-bottom: 2rem;
   }
 `;
 
 export const CartPreview = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
-  justify-content: flex-start;
+  align-items: center;
+  justify-content: center;
   p {
     margin: 0.5rem 0;
+  }
+  @media screen and (max-width: 768px) {
+    margin-bottom: 2rem;
   }
 `;
 
 export const CartButton = styled.button`
   background-color: #f6d53b;
+  font-family: "HarmoniaSansW01-Bold";
   color: #000;
+  max-width: 100%;
   padding: 0.5rem 1rem;
   border: 1px solid black;
   cursor: pointer;
   &:hover {
     background-color: #000;
     color: #f6d53b;
+  }
+`;
+
+export const LogoutButton = styled(CartButton)`
+  /* position: absolute; */
+  background-color: #ce2237;
+  color: #f6d53b;
+  bottom: 0;
+  left: 0;
+  margin: 1rem;
+  @media screen and (max-width: 768px) {
+    margin-top: 2rem;
   }
 `;
 
