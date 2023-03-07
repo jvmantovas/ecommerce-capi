410b8d4754d411be00a83d3b7d67f355d8fe0652
jvmantovas <vmsjoao@gmail.com>
2023-03-07 02:26:03 -0300

    feat: search bar query entry for filter

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,13 +1,32 @@
 import React from "react";
+import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 import { Form, Input, SearchContainer, Submit } from "./styles";
 
 function SearchBar() {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (query.trim() !== "") {
+      navigate(`/products?search=${encodeURIComponent(query)}`);
+      setQuery("");
+    }
+  };
+
   return (
     <div>
       <SearchContainer>
-        <Form action="">
-          <Input type="text" placeholder="Buscar..." name="search" />
+        <Form onSubmit={handleSubmit}>
+          <Input
+            type="text"
+            placeholder="Buscar..."
+            name="search"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
           <Submit type="submit">
             <FaSearch />
           </Submit>
