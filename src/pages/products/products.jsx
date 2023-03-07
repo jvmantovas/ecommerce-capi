99c1729cf25984ed4277dcec3be18380746a69d3
jvmantovas <vmsjoao@gmail.com>
2023-03-07 02:26:43 -0300

    feat: url search params to product grid

--- src/pages/products/products.jsx
+++ src/pages/products/products.jsx
@@ -4,13 +4,17 @@ import { Header } from "../../components/Header/Header";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { ProductGrid } from "../../components/ProductGrid/ProductGrid";
+import { useLocation } from "react-router-dom";
 
 const Products = () => {
+  const location = useLocation();
+  const searchQuery = new URLSearchParams(location.search).get("search");
+
   return (
     <>
       <Header />
       <ToastContainer autoClose={3000} position={toast.POSITION.TOP_RIGHT} />
-      <ProductGrid />
+      <ProductGrid searchQuery={searchQuery} />
       <Footer />
     </>
   );
