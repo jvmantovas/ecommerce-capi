18e62bd7677a55a2e41db3e1b28e41f7e5c4aa52
jvmantovas <vmsjoao@gmail.com>
2023-03-07 23:50:54 -0300

    fix: toast css

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,11 +3,13 @@ import React from "react";
 import { Footer } from "../../components/Footer/Footer";
 import { Header } from "../../components/Header/Header";
 import { Hero } from "../../components/Hero/Hero";
+import { toast, ToastContainer } from "react-toastify";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <ToastContainer autoClose={3000} position={toast.POSITION.TOP_RIGHT} />
       <Hero />
       <Footer />
     </>
