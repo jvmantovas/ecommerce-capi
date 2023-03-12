0c89537b229d62a814cabe63929797a0758de5db
jvmantovas <vmsjoao@gmail.com>
2023-03-11 21:15:48 -0300

    styles: beauty in order menus

--- src/components/SortMenu/SortMenu.jsx
+++ src/components/SortMenu/SortMenu.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { FaLongArrowAltDown, FaLongArrowAltUp } from "react-icons/fa";
 import { SortMenuWrapper } from "./styles";
 
 const SortMenu = ({ handleSort }) => {
   return (
     <SortMenuWrapper>
       <button onClick={() => handleSort("default")}>Padrão</button>
-      <button onClick={() => handleSort("highest")}>Maior para menor</button>
-      <button onClick={() => handleSort("lowest")}>Menor para maior</button>
+      <button onClick={() => handleSort("highest")}>
+        Preço
+        <FaLongArrowAltUp />
+      </button>
+      <button onClick={() => handleSort("lowest")}>
+        Preço
+        <FaLongArrowAltDown />
+      </button>
     </SortMenuWrapper>
   );
 };
