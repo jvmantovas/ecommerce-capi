59037cacdff9f8f405548cf1febf348be610bf4b
jvmantovas <vmsjoao@gmail.com>
2023-03-11 21:16:09 -0300

    fix: toast not showing up

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -3,11 +3,14 @@ import React from "react";
 import { Footer } from "../../components/Footer/Footer";
 import { Header } from "../../components/Header/Header";
 import { Cart } from "../../components/Cart/Cart";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const CartPage = () => {
   return (
     <>
       <Header />
+      <ToastContainer autoClose={3000} position={toast.POSITION.TOP_RIGHT} />
       <Cart />
       <Footer />
     </>
