81a62f0a9bc4bbf63c62bb0f0856fce6273737fe
jvmantovas <vmsjoao@gmail.com>
2023-03-12 23:58:30 -0300

    vite updates

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgx from "@svgx/vite-plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgx()],
   build: {
     minify: false,
   },
