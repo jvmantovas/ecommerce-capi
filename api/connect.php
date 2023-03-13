efb3f3fd1798620075fdf11190e9eac9d7c7ec2a
jvmantovas <vmsjoao@gmail.com>
2023-03-13 01:05:42 -0300

    php connection config

--- api/connect.php
+++ api/connect.php
@@ -1,13 +1,13 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database= "capi";
+$servername = "db4free.net:3306";
+$username = "capirecords";
+$password = "capi654654";
+$database= "capivararecords";
  
 $db = mysqli_connect($servername, $username, $password, $database);
 
 if ($db->connect_error) {
   die("Connection failed: " . $db->connect_error);
 }
-// echo "Connected successfully";
+echo "Connected successfully";
 ?>
